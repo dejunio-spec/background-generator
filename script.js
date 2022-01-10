e6fb0dbfd790560b12a35254f5eada87a076e03d
dejunio-spec <jose.martnnez@uao.edu.co>
2022-01-10 13:46:18 -0500

    Insertando los archivos iniciales del proyecto

---
+++ script.js
@@ -0,0 +1,13 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var button = document.querySelector("button");
+var box = document.querySelector(".result");
+
+function setGradient(){
+    box.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    box.textContent = box.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
